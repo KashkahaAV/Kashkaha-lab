16706fe39e85942f074c95e950d176411e97ed9d
KashkahaAV <gichdte@gmail.com>
2026-03-16 09:15:42 +0300

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,18 @@
+public class Utils {
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
